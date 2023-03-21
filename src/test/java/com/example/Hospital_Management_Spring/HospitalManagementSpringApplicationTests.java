c72ed4241bac75c1a72505606cb9190477f2e21e
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-03-22 00:16:20 +0530

    Hospital API Use Project

---
+++ src/test/java/com/example/Hospital_Management_Spring/HospitalManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hospital_Management_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
